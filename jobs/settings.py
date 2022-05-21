d3e74dc8ca2a3e3bd1dd798cbd674091a863dac8
ZakariaZhlat125 <zakooorr1567@gmail.com>
2022-05-21 20:06:02 +0300

    first1 Heroku

--- jobs/settings.py
+++ jobs/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 #Deploment with hercku
-django_heroku.settings(locals())
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
